a52c806a2778f4bef8f45139afd2c86be20d311a
Cristobal Cuevas <kriistobal1@gmail.com>
2022-11-08 00:51:01 -0300

    add codes from hackerrank

---
+++ PlayingWithCharacters.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+#define MAX_LEN 64
+int main() 
+{
+    char ch;
+    char s[MAX_LEN];
+    char sen[MAX_LEN];
+    scanf("%c\n", &ch);
+    scanf("%s\n", s);
+    scanf("%[^\n]%*c", sen);
+    printf("%c\n", ch);
+    printf("%s\n", s);
+    printf("%s", sen);
+    return 0;
+}
